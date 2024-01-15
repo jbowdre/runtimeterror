cdc261b82e45f8bb2745a0626ba23addcc45b59a
John Bowdre <john@bowdre.net>
2024-01-15 14:00:28 -0600

    enable minify in config, don't minify xml

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Quick script to run local builds
 source .env
-hugo --minify --environment local -D
+hugo --environment local -D
 npx torchlight
 python3 -m http.server --directory public 1313
 
