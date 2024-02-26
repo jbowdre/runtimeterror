c7fd136217ad03f6be46d0730cff40fd9b3ba112
John Bowdre <john@bowdre.net>
2024-02-26 16:54:11 -0600

    add nix flake for nix develop shell with direnv

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "runtimeterror build environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in
+   {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        go
+        hugo
+        nodePackages.npm
+      ];
+    };
+  };
+}
